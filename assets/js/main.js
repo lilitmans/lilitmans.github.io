828cd1ff6d92ed45b88834ae0d8a3a89dd9979ca
Lilit Manucharyan <lilit.mans@gmail.com>
2020-12-23 11:16:27 -0800

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    $('.single-item').slick({
+        prevArrow: `<i class="fas fa-angle-left"></i>`,
+        nextArrow: `<i class="fas fa-angle-right"></i>`
+    });
+})
